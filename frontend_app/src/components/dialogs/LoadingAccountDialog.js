eb863e9656a1720e4e1057c5e2193c746fda5269
Joshua Snider <jsnider@mtu.edu>
2021-06-12 21:30:26 -0400

    Better loading screen

--- frontend_app/src/components/dialogs/LoadingAccountDialog.js
+++ frontend_app/src/components/dialogs/LoadingAccountDialog.js
@@ -2,7 +2,9 @@ import React, { Fragment, useContext } from "react";
 import {
   Dialog,
   DialogContent,
+  Divider,
   CircularProgress,
+  Grid,
 } from "@material-ui/core";
 import Typography from '@material-ui/core/Typography';
 import { cryptography } from "@liskhq/lisk-client";
@@ -22,6 +24,7 @@ export default function LoadingAccountDialog(props) {
       minFeePerByte: nodeInfo.minFeePerByte,
     });
     await api.sendTransactions(res.tx);
+    // Need to replace with a check for account from API
     await new Promise(r => setTimeout(r, 5500));
     props.handleClose(props.address);
   };
@@ -33,7 +36,13 @@ export default function LoadingAccountDialog(props) {
           <Typography component="h1" variant="h5">
             Creating Account
           </Typography>
-          <CircularProgress />
+          <Divider />
+          <br />
+          <Grid container justify="center">
+            <Grid item>
+              <CircularProgress />
+            </Grid>
+          </Grid>
         </DialogContent>
       </Dialog>
     </Fragment>
