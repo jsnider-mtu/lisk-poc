dd3db8dd85e399fb7928888c3e4a267e4dc3abbb
Joshua Snider <jsnider@mtu.edu>
2021-10-09 19:40:49 -0400

    HTTPS Everywhere

--- frontend_app/src/api/index.js
+++ frontend_app/src/api/index.js
@@ -1,17 +1,17 @@
 export const fetchNodeInfo = async () => {
-  return fetch("http://"+window.location.hostname+":4000/api/node/info")
+  return fetch("https://"+window.location.hostname+":4000/api/node/info")
     .then((res) => res.json())
     .then((res) => res.data);
 };
 
 export const fetchAccountInfo = async (address) => {
-  return fetch(`http://`+window.location.hostname+`:4000/api/accounts/${address}`)
+  return fetch(`https://`+window.location.hostname+`:4000/api/accounts/${address}`)
     .then((res) => res.json())
     .then((res) => res.data);
 };
 
 export const sendTransactions = async (tx) => {
-  return fetch("http://"+window.location.hostname+":4000/api/transactions", {
+  return fetch("https://"+window.location.hostname+":4000/api/transactions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -23,19 +23,19 @@ export const sendTransactions = async (tx) => {
 };
 
 export const fetchAllPosts = async () => {
-  return fetch("http://"+window.location.hostname+":8080/api/posts")
+  return fetch("https://"+window.location.hostname+":8080/api/posts")
     .then((res) => res.json())
     .then((res) => res.data);
 };
 
 export const fetchPost = async (id) => {
-  return fetch(`http://`+window.location.hostname+`:8080/api/posts/${id}`)
+  return fetch(`https://`+window.location.hostname+`:8080/api/posts/${id}`)
     .then((res) => res.json())
     .then((res) => res.data);
 };
 
 export const getAllTransactions = async () => {
-  return fetch("http://"+window.location.hostname+":8080/api/transactions")
+  return fetch("https://"+window.location.hostname+":8080/api/transactions")
     .then((res) => res.json())
     .then((res) => {
       return res.data;
