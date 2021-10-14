d67090c3981bd54073de48288e7cce9c17ede0d6
Joshua Snider <jsnider@mtu.edu>
2021-10-13 21:29:27 -0400

    Trying out the new package

--- frontend_app/src/components/AllPage.js
+++ frontend_app/src/components/AllPage.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { fetchAllPosts } from "../api";
 import ScrollTop from "./ScrollTop";
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import LazyLoad from 'react-lazyload';
 import { handleViewport } from 'react-in-viewport';
 
 const useStyles = makeStyles((theme) => ({
@@ -82,14 +83,16 @@ function AllPage() {
       <Fragment>
         <CssBaseline />
         {Posts.map((item) => (
-        <div key={item.id}>
-          <Grid key={item.id} container spacing={1} justify="center">
-              <Grid key={item.id} item md={10}>
-                <Post item={item} key={item.id} minimum={false} />
-              </Grid>
-          </Grid>
-          <br />
-        </div>
+        <LazyLoad key={item.id} placeholder={<CircularProgress />}>
+          <div key={item.id}>
+            <Grid key={item.id} container spacing={1} justify="center">
+                <Grid key={item.id} item md={10}>
+                  <Post item={item} key={item.id} minimum={false} />
+                </Grid>
+            </Grid>
+            <br />
+          </div>
+        </LazyLoad>
         ))}
         <Zoom in={newPosts}>
           <Button
