b392dacb0af5541fe6092aba081ec1836e9087e7
Joshua Snider <jsnider@mtu.edu>
2021-10-13 20:50:23 -0400

    More debugging

--- frontend_app/src/components/PostPage.js
+++ frontend_app/src/components/PostPage.js
@@ -23,6 +23,8 @@ function PostPage() {
       }
       if (post.hasOwnProperty('replies')) {
         let replyArr = [];
+        console.log('replyArr should be empty:');
+        console.log(replyArr);
         replyArr = await Promise.all(
           post.replies.map((a) => fetchPost(a))
         );
