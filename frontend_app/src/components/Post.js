9eac3cffa51ba41455a0be6b4ecf7054de3550de
Joshua Snider <jsnider@mtu.edu>
2021-10-26 16:45:34 -0400

    Trying the other way

--- frontend_app/src/components/Post.js
+++ frontend_app/src/components/Post.js
@@ -531,7 +531,7 @@ export default function Post(props) {
   } else {
     linkpreview =
       <LinkPreview url={props.item.hyperlinks[0]} 
-        backgroundColor={blue[250]}
+        backgroundColor={blue[800]}
         primaryTextColor="white"
         secondaryTextColor="light-gray"
       />;
