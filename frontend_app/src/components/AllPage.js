b519bd2281a0eb4195eec47c8d1cd8efdbfd7e23
Joshua Snider <jsnider@mtu.edu>
2021-11-15 18:15:58 -0500

    Reverse sorting for pinnedposts

--- frontend_app/src/components/AllPage.js
+++ frontend_app/src/components/AllPage.js
@@ -71,10 +71,10 @@ function AllPage() {
       }
       pinnedposts.sort(function(a, b) {
         if (a.timestamp < b.timestamp) {
-          return 1;
+          return -1;
         }
         if (a.timestamp > b.timestamp) {
-          return -1;
+          return 1;
         }
         return 0;
       });
