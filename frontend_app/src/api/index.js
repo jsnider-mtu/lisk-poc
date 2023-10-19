6bed2d7aa923bf69640fb600bb34a169c8651cba
Joshua Snider <jsnider@mtu.edu>
2023-10-19 06:29:33 -0400

    Better syntax

--- frontend_app/src/api/index.js
+++ frontend_app/src/api/index.js
@@ -1,19 +1,21 @@
+const scheme = window.location.hostname === 'localhost' && 'http' || 'https';
+
 export const fetchNodeInfo = async () => {
-  return fetch("https://"+window.location.hostname+":4000/api/node/info")
+  return fetch(`${scheme}://`+window.location.hostname+":4000/api/node/info")
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const fetchAccountInfo = async (address) => {
-  return fetch(`https://`+window.location.hostname+`:4000/api/accounts/${address}`)
+  return fetch(`${scheme}://`+window.location.hostname+`:4000/api/accounts/${address}`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const sendTransactions = async (tx) => {
-  return fetch("https://"+window.location.hostname+":4000/api/transactions", {
+  return fetch(`${scheme}://`+window.location.hostname+":4000/api/transactions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,28 +28,28 @@ export const sendTransactions = async (tx) => {
 };
 
 export const fetchAllPosts = async () => {
-  return fetch("https://"+window.location.hostname+":8080/api/posts")
+  return fetch(`${scheme}://`+window.location.hostname+":8080/api/posts")
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const fetchPost = async (id) => {
-  return fetch(`https://`+window.location.hostname+`:8080/api/posts/${id}`)
+  return fetch(`${scheme}://`+window.location.hostname+`:8080/api/posts/${id}`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const getAllAccounts = async () => {
-  return fetch(`https://`+window.location.hostname+`:8080/api/accounts`)
+  return fetch(`${scheme}://`+window.location.hostname+`:8080/api/accounts`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const getAllTransactions = async () => {
-  return fetch("https://"+window.location.hostname+":8080/api/transactions")
+  return fetch(`${scheme}://`+window.location.hostname+":8080/api/transactions")
     .then((res) => res.json())
     .then((res) => {
       return res.data;
