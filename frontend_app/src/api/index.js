02c7876033286e1b620ea8b01918c7cd5955118e
Joshua Snider <jsnider@mtu.edu>
2023-10-19 07:42:42 -0400

    Use tinyhippo.ninja for API calls in prod

--- frontend_app/src/api/index.js
+++ frontend_app/src/api/index.js
@@ -1,21 +1,29 @@
-const scheme = window.location.hostname === 'localhost' && 'http' || 'https';
+let apihost = window.location.hostname;
+
+if (process.env.NODE_ENV === 'production') {
+  apihost = 'tinyhippo.ninja';
+};
+
+const scheme = apihost === 'tinyhippo.ninja' && 'https' || 'http';
+const apiport = apihost === 'tinyhippo.ninja' && '4001' || '4000';
+const socmedapiport = apihost === 'tinyhippo.ninja' && '8081' || '8080';
 
 export const fetchNodeInfo = async () => {
-  return fetch(`${scheme}://`+window.location.hostname+":4000/api/node/info")
+  return fetch(`${scheme}://${apihost}:${apiport}/api/node/info`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const fetchAccountInfo = async (address) => {
-  return fetch(`${scheme}://`+window.location.hostname+`:4000/api/accounts/${address}`)
+  return fetch(`${scheme}://${apihost}:${apiport}/api/accounts/${address}`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const sendTransactions = async (tx) => {
-  return fetch(`${scheme}://`+window.location.hostname+":4000/api/transactions", {
+  return fetch(`${scheme}://${apihost}:${apiport}/api/transactions`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -28,28 +36,28 @@ export const sendTransactions = async (tx) => {
 };
 
 export const fetchAllPosts = async () => {
-  return fetch(`${scheme}://`+window.location.hostname+":8080/api/posts")
+  return fetch(`${scheme}://${apihost}:${socmedapiport}/api/posts`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const fetchPost = async (id) => {
-  return fetch(`${scheme}://`+window.location.hostname+`:8080/api/posts/${id}`)
+  return fetch(`${scheme}://${apihost}:${socmedapiport}/api/posts/${id}`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const getAllAccounts = async () => {
-  return fetch(`${scheme}://`+window.location.hostname+`:8080/api/accounts`)
+  return fetch(`${scheme}://${apihost}:${socmedapiport}/api/accounts`)
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
 
 export const getAllTransactions = async () => {
-  return fetch(`${scheme}://`+window.location.hostname+":8080/api/transactions")
+  return fetch(`${scheme}://${apihost}:${socmedapiport}/api/transactions`)
     .then((res) => res.json())
     .then((res) => {
       return res.data;
